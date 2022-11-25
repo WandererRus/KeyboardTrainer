5e075b98d2c8ea5a6734ca4027e9e56e05f96e39
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-11-25 21:22:36 +0300

    Добавьте файлы проекта.

---
+++ KeyboardTrainer/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace KeyboardTrainer
+{
+    internal sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string textForTask = "";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            speed.Text= "0";
+            fails.Text= "0";            
+        }
+
+        private void difficult_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelDifficult.Text= ((int)difficult.Value).ToString();
+            taskText.Text= TextGenerator.GetTextByDiffLvl((int)difficult.Value);
+        }
+
+    }
+}
---
+++ KeyboardTrainer/TextGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace KeyboardTrainer
+{
+    internal class TextGenerator
+    {
+        static char[] charsLevelZero = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelOne = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8' };
+        static char[] charsLevelTwo = new char[] { 'r', 't', 'y', 'u', 'f', 'g', ' ', 'h', 'j', 'v', 'b', 'n', 'm', '5', '6', '7', '8', 'e', 'd', 'c', '4', 'i', 'k', ',', '9' };
+        /*static char[] charsLevelThree = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelFour = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelFive = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelSix = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelSeven = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelEight = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelNine = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };
+        static char[] charsLevelTen = new char[] { 'r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n', 'm' };*/
+        static public string GetTextByDiffLvl( int diffLvl)
+        {
+            char[] currentLevelMassive;
+
+            switch (diffLvl)
+            {
+                case 1: currentLevelMassive = charsLevelZero; break;
+                case 2: currentLevelMassive = charsLevelOne; break;
+                case 3: currentLevelMassive = charsLevelTwo; break;
+                default: currentLevelMassive = charsLevelZero; break;
+            }
+            return GenerateText(currentLevelMassive);
+        }
+
+        static string GenerateText(char[] chars)
+        {
+            string resultText= "";
+            Random rnd = new Random();
+            for (int i = 0; i < 70; i++)
+            {
+                resultText += chars[rnd.Next(0, chars.Length)];
+            }
+
+            return resultText;
+        }
+
+    }
+}
